e2509b810ff8c096aa2a3aacdaa69aa49fd5369f
340867-ux <340867@gmail.com>
2026-08-30 18:25:08 +0300

    Moodle: add capability

---
+++ integrations/moodle/local/maglasync_context/db/access.php
@@ -0,0 +1,15 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die();
+
+$capabilities = [
+    'local/maglasync_context:use' => [
+        'riskbitmask' => RISK_PERSONAL,
+        'captype' => 'read',
+        'contextlevel' => CONTEXT_COURSE,
+        'archetypes' => [
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW,
+        ],
+    ],
+];
